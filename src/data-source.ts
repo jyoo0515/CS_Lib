6ce8093ec8d0f607542dc5fa388caab63a1fa6f0
jyoo0515 <jasonyoo0515@gmail.com>
2022-04-10 00:15:33 +0900

    update: orm config separted for dev and prod

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,22 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
 
-export const AppDataSource = new DataSource({
-  type: "sqlite",
-  database: "dev.db",
+const dev = new DataSource({
+  type: 'sqlite',
+  database: 'dev.db',
   synchronize: true,
   logging: true,
-  entities: [__dirname + "/entity/*.ts"],
-  migrations: [__dirname + "/migration/*.ts"],
+  entities: [__dirname + '/entity/*.{ts,js}'],
+  migrations: [__dirname + '/migration/*.{ts,js}'],
 });
+
+const prod = new DataSource({
+  type: 'sqlite',
+  database: 'dev.db',
+  synchronize: false,
+  logging: true,
+  entities: [__dirname + '/entity/*.{ts,js}'],
+  migrations: [__dirname + '/migration/*.{ts,js}'],
+});
+
+export default { dev, prod };
