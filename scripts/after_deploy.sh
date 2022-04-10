2a79bcb9f4d3e44f243a8278fb9f88e4bb4c523f
jyoo0515 <jasonyoo0515@gmail.com>
2022-04-10 21:14:42 +0900

    update: docker settings for production

---
+++ scripts/after_deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/cslib
+cd $REPOSITORY
+
+docker kill $(docker ps -q)
+docker-compose up --build -d
--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-REPOSITORY=/home/ubuntu/cslib
-cd $REPOSITORY
-
-echo "> Start"
-docker kill $(docker ps -q)
-docker-compose build
-docker-compose up -d
