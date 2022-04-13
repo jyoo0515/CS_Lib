3cfd070fa1d7f4f21e53140e686185f95597b534
jyoo0515 <jasonyoo0515@gmail.com>
2022-04-14 01:56:48 +0900

    fix: user route for 'me' fixed

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,7 +4,7 @@ import * as auth from '../middleware/auth';
 const router = express.Router();
 
 router.route('/').all(auth.verifyToken).get(userController.getAll).delete(userController.deleteUser).put(userController.updateUser);
-router.route('/me').get(userController.me);
+router.route('/me').all(auth.verifyToken).get(userController.me);
 router.route('/unique/:username').get(userController.checkUnique);
 router.route('/register').post(userController.register);
 router.route('/login').post(userController.login);
